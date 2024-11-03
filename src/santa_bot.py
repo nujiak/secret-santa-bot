49c4025db6f0c462ebe5e7cde05c1323374e26d4
Chua Kai Jun <chuakaijun.ckj@gmail.com>
2024-11-04 02:08:41 +0800

    Add start and status commands

--- src/santa_bot.py
+++ src/santa_bot.py
@@ -1,28 +1,32 @@
 import asyncio
 from collections.abc import Callable, Awaitable
 from functools import wraps
-from typing import Any
+from typing import Any, Union
 
 from telegram import Update, ChatFullInfo
 from telegram.constants import ChatType, ParseMode
 from telegram.ext import CallbackContext, BaseHandler, CommandHandler, Application, PollAnswerHandler
 
 from literals import JOIN_STRING
+from models import UserId, GroupId
 from models.group import Group
 from stores.store import Store
 from utils import shuffle_pair
 
 
-def restrict_to_group(message: str):
+def restrict_to_chat_type(message: str, chat_types: set[ChatType]):
     def decorator(callback: Callable[[Any, Update, CallbackContext], Awaitable[None]]):
         @wraps(callback)
         async def wrapper(self: Any, update: Update, context: CallbackContext):
-            if update.message.chat.type not in {ChatType.GROUP, ChatType.SUPERGROUP}:
+            if update.message.chat.type not in chat_types:
                 await update.message.reply_text(message)
                 return
             await callback(self, update, context)
+
         return wrapper
+
     return decorator
+
 
 class SantaBot:
     def __init__(self, store: Store, application: Application):
@@ -30,6 +34,9 @@ class SantaBot:
         self.__id = None
         self.__application = application
         application.add_handlers(self._get_handlers())
+
+    async def __get_chat_info(self, chat_id: Union[UserId, GroupId]) -> ChatFullInfo:
+        return await self.__application.bot.get_chat(chat_id)
 
     @staticmethod
     def require_me(callback: Callable[[Any, Update, CallbackContext], Awaitable[None]]):
@@ -42,7 +49,8 @@ class SantaBot:
 
         return wrapper
 
-    @restrict_to_group("Use this command in a group to start new Secret Santa game")
+    @restrict_to_chat_type("Use this command in a group to start new Secret Santa game",
+                           {ChatType.GROUP, ChatType.SUPERGROUP})
     async def _handle_new(self, update: Update, _: CallbackContext):
         splits = update.message.text.split(" ", 1)
         if len(splits) == 1:
@@ -57,14 +65,15 @@ class SantaBot:
             return
 
         message = await update.message.chat.send_poll(
-                question=new_game_name,
-                options=[JOIN_STRING, "No thanks"],
-                is_anonymous=False,
+            question=new_game_name,
+            options=[JOIN_STRING, "No thanks"],
+            is_anonymous=False,
         )
         await self.__store.create_game(new_game_name, group, message.poll.id)
 
     @require_me
-    @restrict_to_group("Use this command in a group to shuffle a Secret Santa game")
+    @restrict_to_chat_type("Use this command in a group to shuffle a Secret Santa game",
+                           {ChatType.GROUP, ChatType.SUPERGROUP})
     async def _handle_shuffle(self, update: Update, _: CallbackContext):
         if (not update.message.reply_to_message
                 or not update.message.reply_to_message.poll
@@ -77,16 +86,16 @@ class SantaBot:
         pairings = shuffle_pair(users)
         await self.__store.save_pairings(poll_id, pairings)
 
-        group: ChatFullInfo = await self.__application.bot.get_chat(game.group_id)
+        group: ChatFullInfo = await self.__get_chat_info(game.group_id)
 
         async def update_user(santa_id, recipient_id):
-            recipient: ChatFullInfo = await self.__application.bot.get_chat(recipient_id)
+            recipient: ChatFullInfo = await self.__get_chat_info(recipient_id)
             recipient_name = f"{recipient.first_name or ""} {recipient.last_name or ''}".strip() or "Unnamed"
             await self.__application.bot.send_message(
                 santa_id,
                 " ".join([f"You have been assigned as [{recipient_name}](tg://user?id={recipient_id})'s",
                           rf"\(@{recipient.username}\)" if recipient.username else "",
-                          f"Secret Santa for '__{game.name}__' in *{group.title}*!"]),
+                          rf"Secret Santa for '__{game.name}__' in *{group.title}*\!"]),
                 parse_mode=ParseMode.MARKDOWN_V2
             )
 
@@ -98,9 +107,32 @@ class SantaBot:
         else:
             await self.__store.remove_user_from_game(update.poll_answer.user.id, update.poll_answer.poll_id)
 
+    @restrict_to_chat_type("Send this to me as a private message instead",
+                           {ChatType.PRIVATE})
+    async def _handle_status(self, update: Update, _: CallbackContext):
+        user_id = update.message.id
+        pairings = await self.__store.get_pairings(user_id)
+        if not pairings:
+            message = r"You are currently not in any Secret Santas\!"
+        else:
+            message = "\n".join(["These are whom you are the Secret Santa for:\n",
+                                 *(rf"__{game.name}__ \(*{(await self.__get_chat_info(game.group_id)).title}*\)"
+                                   for game, recipient_id in pairings)])
+        await update.message.reply_markdown_v2(message)
+
+    @restrict_to_chat_type(
+        "Use /new to start a new Secret Santa here.\n\nSend /start to me as a private message for more information.",
+        {ChatType.PRIVATE})
+    async def _handle_start(self, update: Update, callback_context: CallbackContext):
+        await update.message.reply_text(
+            "Welcome to the Secret Santa Bot! I can help you to organise a Secret Santa in a group, just add me to the group and send /new")
+        await self._handle_status(update, callback_context)
+
     def _get_handlers(self) -> list[BaseHandler]:
         return [
             CommandHandler("new", self._handle_new),
             CommandHandler("shuffle", self._handle_shuffle),
+            CommandHandler(["status"], self._handle_status),
+            CommandHandler(["start"], self._handle_start),
             PollAnswerHandler(self._handle_poll_answer),
         ]
